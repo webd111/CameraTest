d2e6a6f5ca7abbaf116d6b525db4623a22d13829
webd11 <hkj7531@foxmail.com>
2019-09-09 22:20:10 +0800

    Deleted send command in every loop for efficency

--- thread/imgacqthread.cpp
+++ thread/imgacqthread.cpp
@@ -162,9 +162,10 @@ void ImgAcqThread::run()
 
             // Using socket
             Mat img;
+            webCamera->sendCommand(0x01);           // cmd: send. The send procedure seems to be necessary before it can start to receive.
             while( !isInterruptionRequested() )
             {
-                webCamera->sendCommand(0x01);           // cmd: send
+//                webCamera->sendCommand(0x01);           // cmd: send
                 while(!webCamera->grabSocket() && !isInterruptionRequested());
 //                webCamera->sendCommand(0x02);           // cmd: received
                 if (!webCamera->getImage(img))
@@ -196,7 +197,6 @@ void ImgAcqThread::run()
     {
         qDebug() << "ImgAcqThread::run() wrong cameraInterface!";
     }
-
 }
 
 
